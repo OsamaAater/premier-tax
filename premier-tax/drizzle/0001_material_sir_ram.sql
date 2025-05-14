4393b5af3e82d12f3ea5c10b73ca462b11425e84
Osama Aater <77805090+OsamaAater@users.noreply.github.com>
2025-05-14 15:35:10 -0500

    Delete premier-tax directory

--- premier-tax/drizzle/0001_material_sir_ram.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "premier-tax_user" ADD COLUMN "password" varchar(255);--> statement-breakpoint
-ALTER TABLE "premier-tax_user" ADD CONSTRAINT "premier-tax_user_email_unique" UNIQUE("email");
